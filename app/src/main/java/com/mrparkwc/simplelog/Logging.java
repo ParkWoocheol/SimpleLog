c4de1422ebb7d42f39f55b805e35005e8d2bf37a
ParkWoocheol <magic1wp@naver.com>
2015-11-01 21:25:02 +0900

    version 0.9.2 update

--- app/src/main/java/com/mrparkwc/simplelog/Logging.java
+++ app/src/main/java/com/mrparkwc/simplelog/Logging.java
@@ -19,6 +19,7 @@ package com.mrparkwc.simplelog;
 
 import android.util.Log;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 
 /**
@@ -63,6 +64,45 @@ public class Logging {
         }
     }
 
+    /**
+     * if need Class inner variable access modifier set default, protected, public
+     * Print Form : Class Name : [Object Name], Variable Name, Value
+     * @param object
+     * @throws Exception
+     */
+    public static void d(Object object) throws Exception {
+        Class<?> objClass = object.getClass();
+        Field[] fields = objClass.getDeclaredFields();
+        for (Field field : fields) {
+            String name = field.getName();
+            Object value = field.get(object);
+            Log.d(getClassName(Thread.currentThread().getStackTrace()[3].getClassName()),
+                    "[ " + object.getClass().getSimpleName() + " ], " + "Variable Name: " + name + ", Value: " + value);
+        }
+    }
+
+    /**
+     * if need Class inner variable access modifier set default, protected, public
+     * Print Form : Class Name : [Object Name] [ArrayList Index], Variable Name, Value
+     * @param arrayList
+     * @throws Exception
+     */
+    public static void d(ArrayList<?> arrayList) throws Exception {
+        Object object;
+        for (int i = 0; i < arrayList.size(); i++) {
+            object = arrayList.get(i);
+            Class<?> objClass = object.getClass();
+            Field[] fields = objClass.getDeclaredFields();
+            for (Field field : fields) {
+                String name = field.getName();
+                Object value = field.get(object);
+                Log.d(getClassName(Thread.currentThread().getStackTrace()[3].getClassName()),
+                        "[ " + object.getClass().getSimpleName() + " ][ " + i + " ], " + "Variable Name: " + name + ", Value: " + value);
+            }
+        }
+    }
+
+
 
     /**
      * Log Info
@@ -96,6 +136,44 @@ public class Logging {
         for (int i = 0; i < arrayList.size(); i++) {
             Log.i(getClassName(Thread.currentThread().getStackTrace()[3].getClassName()), "METHOD: " + Thread.currentThread().getStackTrace()[3].getMethodName()
                     + ", LINE: " + Thread.currentThread().getStackTrace()[3].getLineNumber() + ", " + dataTag + " [" + i + "] :" + arrayList);
+        }
+    }
+
+    /**
+     * if need Class inner variable access modifier set default, protected, public
+     * Print Form : Class Name : [Object Name], Variable Name, Value
+     * @param object
+     * @throws Exception
+     */
+    public static void i(Object object) throws Exception {
+        Class<?> objClass = object.getClass();
+        Field[] fields = objClass.getDeclaredFields();
+        for (Field field : fields) {
+            String name = field.getName();
+            Object value = field.get(object);
+            Log.i(getClassName(Thread.currentThread().getStackTrace()[3].getClassName()),
+                    "[ " + object.getClass().getSimpleName() + " ], " + "Variable Name: " + name + ", Value: " + value);
+        }
+    }
+
+    /**
+     * if need Class inner variable access modifier set default, protected, public
+     * Print Form : Class Name : [Object Name] [ArrayList Index], Variable Name, Value
+     * @param arrayList
+     * @throws Exception
+     */
+    public static void i(ArrayList<?> arrayList) throws Exception {
+        Object object;
+        for (int i = 0; i < arrayList.size(); i++) {
+            object = arrayList.get(i);
+            Class<?> objClass = object.getClass();
+            Field[] fields = objClass.getDeclaredFields();
+            for (Field field : fields) {
+                String name = field.getName();
+                Object value = field.get(object);
+                Log.i(getClassName(Thread.currentThread().getStackTrace()[3].getClassName()),
+                        "[ " + object.getClass().getSimpleName() + " ][ " + i + " ], " + "Variable Name: " + name + ", Value: " + value);
+            }
         }
     }
 
@@ -134,6 +212,44 @@ public class Logging {
         }
     }
 
+    /**
+     * if need Class inner variable access modifier set default, protected, public
+     * Print Form : Class Name : [Object Name], Variable Name, Value
+     * @param object
+     * @throws Exception
+     */
+    public static void w(Object object) throws Exception {
+        Class<?> objClass = object.getClass();
+        Field[] fields = objClass.getDeclaredFields();
+        for (Field field : fields) {
+            String name = field.getName();
+            Object value = field.get(object);
+            Log.w(getClassName(Thread.currentThread().getStackTrace()[3].getClassName()),
+                    "[ " + object.getClass().getSimpleName() + " ], " + "Variable Name: " + name + ", Value: " + value);
+        }
+    }
+
+    /**
+     * if need Class inner variable access modifier set default, protected, public
+     * Print Form : Class Name : [Object Name] [ArrayList Index], Variable Name, Value
+     * @param arrayList
+     * @throws Exception
+     */
+    public static void w(ArrayList<?> arrayList) throws Exception {
+        Object object;
+        for (int i = 0; i < arrayList.size(); i++) {
+            object = arrayList.get(i);
+            Class<?> objClass = object.getClass();
+            Field[] fields = objClass.getDeclaredFields();
+            for (Field field : fields) {
+                String name = field.getName();
+                Object value = field.get(object);
+                Log.w(getClassName(Thread.currentThread().getStackTrace()[3].getClassName()),
+                        "[ " + object.getClass().getSimpleName() + " ][ " + i + " ], " + "Variable Name: " + name + ", Value: " + value);
+            }
+        }
+    }
+
 
     /**
      * Log Error
@@ -169,6 +285,45 @@ public class Logging {
                     + ", LINE: " + Thread.currentThread().getStackTrace()[3].getLineNumber() + ", " + dataTag + " [" + i + "] :" + arrayList);
         }
     }
+
+    /**
+     * if need Class inner variable access modifier set default, protected, public
+     * Print Form : Class Name : [Object Name], Variable Name, Value
+     * @param object
+     * @throws Exception
+     */
+    public static void e(Object object) throws Exception {
+        Class<?> objClass = object.getClass();
+        Field[] fields = objClass.getDeclaredFields();
+        for (Field field : fields) {
+            String name = field.getName();
+            Object value = field.get(object);
+            Log.i(getClassName(Thread.currentThread().getStackTrace()[3].getClassName()),
+                    "[ " + object.getClass().getSimpleName() + " ], " + "Variable Name: " + name + ", Value: " + value);
+        }
+    }
+
+    /**
+     * if need Class inner variable access modifier set default, protected, public
+     * Print Form : Class Name : [Object Name] [ArrayList Index], Variable Name, Value
+     * @param arrayList
+     * @throws Exception
+     */
+    public static void e(ArrayList<?> arrayList) throws Exception {
+        Object object;
+        for (int i = 0; i < arrayList.size(); i++) {
+            object = arrayList.get(i);
+            Class<?> objClass = object.getClass();
+            Field[] fields = objClass.getDeclaredFields();
+            for (Field field : fields) {
+                String name = field.getName();
+                Object value = field.get(object);
+                Log.i(getClassName(Thread.currentThread().getStackTrace()[3].getClassName()),
+                        "[ " + object.getClass().getSimpleName() + " ][ " + i + " ], " + "Variable Name: " + name + ", Value: " + value);
+            }
+        }
+    }
+
 
     /**
      * @param packageName
